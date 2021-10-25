8fd3ef199122a6f2e7924df50b44142af179a193
Joy Rhee <joyfullyx@gmail.com>
2021-10-25 11:59:41 -0700

    add dependencies and packages

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const IndexSceen = () => {
+  return (
+    <View>
+      <Text>Index Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default IndexSceen;
